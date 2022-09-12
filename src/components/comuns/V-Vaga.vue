c4facdde4cc9dd3d1f4731e2c3c2cbba041e5b67
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-09-12 11:59:50 -0300

    implementação vaga

---
+++ src/components/comuns/V-Vaga.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card-header bg-dark text-white">Titulo da vaga</div>
+    <div class="card-body">
+      <p>Descrição truncada da vaga...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-vaga', 
+};
+</script>
+
+<style>
+</style>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -7,6 +7,18 @@
     </div>
 
     <div class="row mt-5">
+      <div class="col">
+        <v-vaga></v-vaga>
+        <div class="card-footer">
+          <small class="text-mutted"
+            >Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT |
+            Publicação: 10/11/2021
+          </small>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5">
       <div class="col-4">
         <v-indicador
           titulo="Vagas abertas"
@@ -29,7 +41,7 @@
       <div class="col-4">
         <v-indicador
           titulo="Visitantes online"
-          indicador="25"
+          :indicador="usuariosOnline"
           bg="bg-dark"
           color="text-white"
         ></v-indicador>
@@ -44,6 +56,20 @@ export default {
   components: {
     PesquisarVaga: () => import("@/components/comuns/PesquisarVaga.vue"),
     VIndicador: () => import("@/components/comuns/V-Indicador.vue"),
+    VVaga: () => import("@/components/comuns/V-Vaga.vue"),
+  },
+  data() {
+    return {
+      usuariosOnline: 0,
+    };
+  },
+  methods: {
+    getsUsuariosOnline() {
+      this.usuariosOnline = Math.floor(Math.random() * 101); // entre 0 e 100
+    },
+  },
+  created() {
+    setInterval(this.getsUsuariosOnline, 1000); // cada 1 segundo
   },
 };
 </script>
